5015a3356d5ee767bf2aa4b68765024693f4f5b4
Harald Wellmann <harald.wellmann@gmx.de>
2014-12-31 11:09:12 +0100

    manual init, since @PostConstruct does not work with Sisu

--- pax-warp-jaxb/src/main/java/org/ops4j/pax/warp/jaxb/WarpJaxbContext.java
+++ pax-warp-jaxb/src/main/java/org/ops4j/pax/warp/jaxb/WarpJaxbContext.java
@@ -22,6 +22,7 @@ import static javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI;
 import java.io.IOException;
 import java.net.URL;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Singleton;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -32,8 +33,10 @@ import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 
+import org.ops4j.pax.warp.exc.WarpException;
 import org.ops4j.pax.warp.jaxb.gen.ChangeLog;
 import org.ops4j.pax.warp.scope.CdiApplicationScoped;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.xml.sax.SAXException;
 
@@ -42,16 +45,26 @@ import org.xml.sax.SAXException;
 @CdiApplicationScoped
 public class WarpJaxbContext {
 
-    protected JAXBContext context;
-    protected Schema schema;
+    private JAXBContext context;
+    private Schema schema;
+    private boolean initialized;
 
-    public WarpJaxbContext() {
+    @Activate
+    @PostConstruct
+    public void init() {
         try {
             context = JAXBContext.newInstance(ChangeLog.class);
             loadSchema();
+            initialized = true;
         }
         catch (JAXBException | SAXException | IOException exc) {
-            throw new IllegalStateException(exc);
+            throw new WarpException(exc);
+        }
+    }
+
+    private void initIfNeeded() {
+        if (!initialized) {
+            init();
         }
     }
 
@@ -62,18 +75,21 @@ public class WarpJaxbContext {
     }
 
     public Unmarshaller createValidatingUnmarshaller() throws JAXBException {
+        initIfNeeded();
         Unmarshaller unmarshaller = context.createUnmarshaller();
         unmarshaller.setSchema(schema);
         return unmarshaller;
     }
 
     public Marshaller createValidatingMarshaller() throws JAXBException {
+        initIfNeeded();
         Marshaller marshaller = context.createMarshaller();
         marshaller.setSchema(schema);
         return marshaller;
     }
 
     public Marshaller createFragmentMarshaller() throws JAXBException {
+        initIfNeeded();
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
         return marshaller;
