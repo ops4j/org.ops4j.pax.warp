79def02209dda1a15fee696571c4a4979e63a49a
Harald Wellmann <harald.wellmann@gmx.de>
2016-04-06 15:47:18 +0200

    [PAXWARP-9] Maven goal import-data

---
+++ maven/warp-maven-plugin/src/main/java/org/ops4j/pax/warp/maven/ImportDataMojo.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015 Harald Wellmann.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.warp.maven;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+
+/**
+ * @author hwellmann
+ *
+ */
+@Mojo(name = "import-data", defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST)
+public class ImportDataMojo extends AbstractWarpMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+        try (InputStream is = new FileInputStream(changeLog)) {
+            commandRunner.importData(url, username, password, is);
+        }
+        catch (IOException exc) {
+            throw new MojoExecutionException("error reading change log", exc);
+        }
+    }
+}
